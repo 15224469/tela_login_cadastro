6062efced8148a924535033b1df6d5ab865a7378
15224469 <brendarafaely33@gmail.com>
2024-06-14 16:49:56 -0300

    Junção de ambas as telas

--- Telalogin.py
+++ Telalogin.py
@@ -3,6 +3,7 @@ from kivy.core.window import Window
 from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
+from kivy.utils import get_color_from_hex
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image, AsyncImage
@@ -19,46 +20,150 @@ class FloatApp(App):
             )
         flo.add_widget(icon_login)
 
-        l1 = Label(text='Login', size_hint=(.2, .1), pos=(100, 200))
-        self.t1 = TextInput(size_hint=(.2, .05), pos=(780, 490))
-        flo.add_widget(l1)
+        
+        self.t1 = TextInput (hint_text= 'Login',
+                             font_name='Georgia',
+                             size_hint=(.2, .05),
+                             pos=(780, 580))
         flo.add_widget(self.t1)
 
-        l2 = Label(text="Senha", size_hint=(.2, .1), pos=(100, 200), color="ffa4dd")
-        t2 = TextInput(multiline=True, size_hint=(.2, .05), pos=(780, 380))
-        flo.add_widget(l2)
-        flo.add_widget(t2)
+        self.t2 = TextInput(hint_text="Senha",
+                    font_name='Georgia', 
+                    multiline=False, 
+                    password=True,
+                    size_hint=(.2, .05),
+                    pos=(780, 520)
+                   )
+        flo.add_widget(self.t2)
+
 
         b1 = Button(
-            text='Entrar', size_hint=(.3, .1),
-            pos_hint={'center_x': .5, 'center_y': .07},
+            text='Entrar', size_hint=(.10, .05),
+            pos_hint={'center_x': .5, 'center_y': .45},
+            background_color=get_color_from_hex('ff0066'),
             on_press=self.entrar
         )
         b2 = Button(
-            text='Cadastrar', size_hint=(.3, .1),
-            pos_hint={'center_x': .5, 'center_y': .20},
-            on_press=self.cadastrar
+            text='Cadastrar', size_hint=(.10, .05),
+            pos_hint={'center_x': .5, 'center_y': .40},
+            background_color=get_color_from_hex('ff0066'),
         )
+        b2.bind(on_press=self.ir_para_tela_de_cadastro)
+        self.add_widget(b2)
 
         self.label_cadastrar = Label(
             pos_hint={'center_x': .50, 'center_y': .30},
             color=[1, 1, 1, 1]
         )
 
-        flo.add_widget(b1)
-        flo.add_widget(b2)
-        flo.add_widget(self.label_cadastrar)
+    def ir_para_tela_de_cadastr (self, instanse):
+        
+    
+class Cadastro(FloatLayout):
+    def __init__(self, **kwargs):
+        super(Cadastro, self).__init__(**kwargs)
+        self.orientation='horizontal'
+        self.spacing=5
+        self.padding=[20, 10]
+        Window.clearcolor = get_color_from_hex('d0417e') 
 
-        return flo
+        self.label = Label(
+            text= 'CADASTRO:', 
+            font_size = 40, 
+            font_name = 'Georgia',
+            size_hint_y=None,
+            pos_hint={'x': .013, "y": .8}
+            )
+        self.add_widget(self.label)
 
-    def cadastrar(self, instance):
-        login = self.t1.text
-        mensagem = f'O Login {login} foi cadastrado!'
-        self.label_cadastrar.text = mensagem
+        self.nome = TextInput(
+            hint_text="Digite Seu Nome Completo: ",
+            size_hint=[.4, .1],
+            multiline=False,
+            pos_hint= {'x': .3, "y": .7},
+            padding_y= [10, 10],
+            padding_x= [10, 10],
+            background_color=get_color_from_hex('ffdde8'),  
+            background_normal='',  
+            border=(1, 1, 1, 1)  
+        )
+        self.add_widget(self.nome)
 
-    def entrar(self, instance):
-        login = self.t1.text
-        mensagem = f'O login {login} acessou!'
-        self.label_cadastrar.text = mensagem
+        self.email = TextInput(
+            hint_text="Digite um E-mail Válido: ",
+            size_hint=[.4, .1],
+            multiline=False,
+            pos_hint= {'x': .3, "y": .5},
+            padding_y= [10, 10],
+            padding_x= [10, 10],
+            background_color=get_color_from_hex('ffdde8'),  
+            background_normal='',  
+            border=(1, 1, 1, 1)  
+        )
+        self.add_widget(self.email)
+
+        self.celular = TextInput(
+            hint_text="Digite Seu Número de telefone: ",
+            size_hint=[.4, .1],
+            multiline=False,
+            pos_hint= {'x': .3, "y": .3},
+            padding_y= [10, 10],
+            padding_x= [10, 10],
+            background_color=get_color_from_hex('ffdde8'),  
+            background_normal='',  
+            border=(1, 1, 1, 1)  
+        )
+        self.add_widget(self.celular)
+
+        self.senha = TextInput(
+            hint_text="Senha: ",
+            password=True,
+            size_hint=(.4, .1),
+            multiline=False,
+            pos_hint= {'x': .3, "y": .1},
+            padding_y= [10, 10],
+            padding_x= [10, 10],
+            background_color=get_color_from_hex('ffdde8'),  
+            background_normal='',
+            border=(1, 1, 1, 1)  
+        )
+        self.add_widget(self.senha)
+
+        self.button = Button(
+            size_hint = (.1, .05),
+            pos_hint = {'x': .6, 'y':.13} ,
+            text = 'Ver Senha',
+            background_color = get_color_from_hex('ffdde8'),  
+            background_normal='', 
+            on_press = self.togglevisibility 
+        )
+
+        self.button2 = Button(
+            size_hint = (.1, .05),
+            pos_hint = {'x': .45, 'y':.04} ,
+            text = 'Cadastrar',
+            background_color = ('ffdde8')
+              )
+        self.add_widget(self.button2)
+
+        self.add_widget(self.button)
+
+    def togglevisibility(self, instance):
+        if self.senha.password == True:
+            self.senha.password = False
+            self.button.text = 'Hide'   
+        else:
+            self.senha.password = True
+            self.button.text = 'Show'   
+
+
+
+
+
+class TelaCadastro(App):
+    def build(self):
+        return Cadastro()
+
+TelaCadastro().run()
 
 FloatApp().run()
